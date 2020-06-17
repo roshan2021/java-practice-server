249a557b9696992b77b0ac1b820934daa8a0fcd7
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-06-16 20:01:10 -0500

    updated RegisterServlet to validate unique username from daos

--- src/main/java/controllers/RegisterServlet.java
+++ src/main/java/controllers/RegisterServlet.java
@@ -26,7 +26,8 @@ public class RegisterServlet extends HttpServlet {
         boolean inputHasErrors = username.isEmpty()
                 || email.isEmpty()
                 || password.isEmpty()
-                || (! password.equals(passwordConfirmation));
+                || DaoFactory.getUsersDao().validateUsername(username)
+                || (!password.equals(passwordConfirmation));
 
         if (inputHasErrors) {
             response.sendRedirect("/register");
