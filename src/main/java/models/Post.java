af09cb43883fc79d116ec9756f12c3f2955aef67
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-07-25 21:39:16 -0500

    building properties for Post model

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,5 +1,25 @@
 package models;
 
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "posts")
 public class Post {
 
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String img;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String body;
+
+    @DateTimeFormat
+    private Date dateTime;
